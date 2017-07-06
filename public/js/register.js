812d61cf36273396e853a07a70d60a0ffbff911f
Prasanna Deshappriya <prasanna.14@cse.mrt.ac.lk>
2017-07-06 23:02:45 +0530

    Code improvements and bug fixes

--- public/js/register.js
+++ public/js/register.js
@@ -5,31 +5,40 @@ app.controller('LoginController',['$scope','$http', function ($scope, $http) {
     $scope.isEmailExist = false;
     $scope.isComplete = false;
     $scope.submitted = false;
+    $scope.repass = false;
+
+    $scope.setRePass = function (repassword) {
+        if(repassword==="" || typeof repassword==="undefined"){$scope.repass = false;}
+        else {$scope.repass = true;}
+    };
 
     $scope.resetSubmitted = function () {
         $scope.submitted = false;
     };
 
-    $scope.register = function () {
-        $scope.submitted = true;
+    $scope.validate = function () {
+        if(typeof $scope.firstname==="undefined" || $scope.firstname===""){$scope.isComplete = false;return;}
+        if(typeof $scope.lastname==="undefined" || $scope.lastname===""){$scope.isComplete = false;return;}
+        if(typeof $scope.email==="undefined" || $scope.email===""){$scope.isComplete = false;return;}
+        if(typeof $scope.password==="undefined" || $scope.password===""){$scope.isComplete = false;return;}
+        if($scope.password !== $scope.repassword){$scope.isComplete = false;return;}
+        if(typeof $scope.chkTerms==="undefined" || !($scope.chkTerms)){$scope.isComplete = false;return;}
+        $scope.isComplete = true;
     };
 
-    // $scope.isEmailExist = function (email) {
-    //     $http({
-    //         method: "GET",
-    //         url: "http://localhost:3000/auth/register/isexist",
-    //         params: {email: email},
-    //         headers: {'Content-Type': 'application/json'}
-    //     }).then(function (resData){
-    //         if(typeof resData.data.status==="undefined"){return false;}
-    //         if(resData.data.status==="true"){return true;}
-    //         if(resData.data.status==="false"){return true;}
-    //     },function (error){
-    //         $scope.message = "Something fail";
-    //     });
-    // };
-
-    $scope.Test = function (test) {
-        $scope.message = test;
+    $scope.register = function () {
+        if(!($scope.regForm.email.$valid)) {return;}
+        $http({
+            method: "GET",
+            url: "http://localhost:3000/auth/register/isexist",
+            params: {email: email},
+            headers: {'Content-Type': 'application/json'}
+        }).then(function (resData){
+            if(typeof resData.data.status==="undefined"){return false;}
+            if(resData.data.status==="true"){return true;}
+            if(resData.data.status==="false"){return true;}
+        },function (error){
+            $scope.message = "Something fail";
+        });
     };
 }]);
