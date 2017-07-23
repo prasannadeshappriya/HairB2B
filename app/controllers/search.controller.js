bdc5f231513df97235c5f10f6bc4acf5b510fd6c
Prasanna Deshappriya <prasanna.14@cse.mrt.ac.lk>
2017-07-24 00:53:35 +0530

    Implement search function

--- app/controllers/search.controller.js
+++ app/controllers/search.controller.js
@@ -9,6 +9,7 @@ app.controller('SearchController',
                 "Wigs cutting", "Curling", "Coloring", "Color correction", "Long hair", "Short hair", "Select All"];
             $scope.selectedName = $scope.job_types[0];
             $scope.selectedSkillName = $scope.skill_types[0];
+            $scope.empty_results = false;
 
             $scope.beginSearch = function (skill, jobtype) {
                 console.log($scope.job_types.indexOf(skill));
@@ -19,34 +20,57 @@ app.controller('SearchController',
             $scope.search_results = [];
             $scope.onInit = function () {
                 var params = $location.search();
-                var job_type = params.jobtype;
-                var skill_type = params.skilltype;
+                var job_type = (params.jobtype+1);
+                var skill_type = (params.skilltype+1);
                 console.log(job_type);
                 console.log(skill_type);
-                //Hard Code two dummy data
-                $scope.search_results.push({
-                    firstname: "Prasanna",
-                    lastname: "Deshappriya",
-                    acctypes: "Educator, Stylist, Assistant",
-                    price: "$2400",
-                    rates: "0.0",
-                    location: "Sidney",
-                    description: "this is a test this is a test this is a test this is a test this is a test " +
-                    "this is a test this is a test this is a test this is a test this is a test this is a test this is a test " +
-                    "this is a test this is a test this is a test this is a test this is a test this is a test " +
-                    "this is a test this is a test this is a test this is a test this is a test this is a test "
-                });
-                $scope.search_results.push({
-                    firstname: "Hiruni",
-                    lastname: "Kalanika",
-                    acctypes: "Educator, Assistant",
-                    price: "$300",
-                    rates: "0.0",
-                    location: "Sidney",
-                    description: "I'm ma girl I'm ma girl I'm ma girl I'm ma girl I'm ma girl I'm ma girl " +
-                    "I'm ma girl I'm ma girl I'm ma girl I'm ma girl I'm ma girl I'm ma girl I'm ma girl I'm ma girl " +
-                    "I'm ma girl I'm ma girl I'm ma girl I'm ma girl I'm ma girl I'm ma girl I'm ma girl " +
-                    "I'm ma girl I'm ma girl I'm ma girl I'm ma girl I'm ma girl I'm ma girl I'm ma girl I'm ma girl  "
+
+                var query1;
+                var query2;
+                if(job_type==4){query1 = 'typeid=all';}
+                else{query1 = 'typeid=' + job_type;}
+                if(skill_type==12){query2 = 'skillid=all';}
+                else{query2 = 'skillid=' + skill_type;}
+                console.log("search url: http://localhost:3000/search/simplesearch?"+ query2 + "&" + query1);
+                $http({
+                    method: "GET",
+                    url: "http://localhost:3000/search/simplesearch?"+ query2 + "&" + query1
+                }).then(function (resData){
+                    console.log(resData);
+                    var users = resData.data.users;
+                    var stylists = resData.data.stylists;
+                    var userTypes = resData.data.user_types;
+                    var id; var description;
+                    for(var i=0; i<users.length; i++){
+                        if(typeof stylists[i].id==="undefined"){id = stylists[i][0].id;}
+                        else{ id = stylists[i].id;}
+                        if(typeof stylists[i].description==="undefined"){description = stylists[i][0].description;}
+                        else{ description = stylists[i].description;}
+                        var types= '';
+                        for(var j=0; j<userTypes.length; j++){
+                            if(userTypes[j].user_id===id){
+                                if(types===''){
+                                    types=$scope.job_types[(userTypes[j].job_id)-1];
+                                }else{
+                                    types=types + ', ' + $scope.job_types[(userTypes[j].job_id)-1];
+                                }
+                            }
+                        }
+                        $scope.search_results.push({
+                            firstname: users[i][0].firstname,
+                            lastname: users[i][0].lastname,
+                            acctypes: types,
+                            price: "$2400",
+                            rates: "0.0",
+                            location: "Sidney",
+                            description: description
+                        });
+                    }
+                    if($scope.search_results.length===0){
+                        $scope.empty_results = true;
+                    }else{$scope.empty_results = false;}
+                },function (error){
+                    console.log('Error on searching profile: ' + error);
                 });
             };
 
