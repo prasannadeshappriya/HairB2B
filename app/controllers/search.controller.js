d012e859e8af8b5785afd8c238855bee036c4d59
Prasanna Deshappriya <prasanna.14@cse.mrt.ac.lk>
2017-07-28 17:02:25 +0530

    Code improvements

--- app/controllers/search.controller.js
+++ app/controllers/search.controller.js
@@ -4,19 +4,37 @@
 app.controller('SearchController',
     ['$scope','$http','AuthService','$location','host_url',
         function ($scope,$http,AuthService,$location,host_url) {
-            $scope.job_types = ["Stylist", "Educator", "Assistant", "Select All"];
-            $scope.skill_types = ["Barber", "Makeup", "Dry cutting", "Shaving", "Hair styling",
-                "Wigs cutting", "Curling", "Coloring", "Color correction", "Long hair", "Short hair", "Select All"];
-            $scope.selectedName = $scope.job_types[0];
-            $scope.selectedSkillName = $scope.skill_types[0];
+            $scope.job_types = [{name: "Stylist", value: false}, {name: "Educator", value: false},
+                {name: "Assistant", value: false}, {name: "Select All", value: false}];
+
+            $scope.skill_types = [{name: "Barber",value: false}, {name: "Makeup",value: false}, {name: "Dry cutting",value: false},
+                {name: "Shaving",value: false}, {name: "Hair styling",value: false}, {name: "Wigs cutting",value: false},
+                {name: "Curling",value: false}, {name: "Coloring",value: false}, {name: "Color correction",value: false},
+                {name: "Long hair",value: false}, {name: "Short hair",value: false}, {name: "Select All",value: false}];
+
+            $scope.selectedName = $scope.job_types[0].name;
+            $scope.selectedSkillName = $scope.skill_types[0].name;
             $scope.empty_results = false;
 
+            $scope.test = function (test) {
+                // console.log('asdasda: ' + test);
+                    // console.log($scope.job);
+            };
+
             $scope.beginSearch = function (skill, jobtype) {
-                var jobtypes, skilltype;
-                if($scope.job_types.indexOf(jobtype)===3){jobtypes = 'all'}
-                else{jobtypes = ($scope.job_types.indexOf(jobtype)+1)}
-                if($scope.skill_types.indexOf(skill)===11){skilltype = 'all'}
-                else{skilltype = ($scope.skill_types.indexOf(skill)+1)}
+                var jobtypes, skilltype, index;
+                for(index=0; index<$scope.job_types.length; index++){
+                    if($scope.job_types[index].name===jobtype){
+                        if(index===3){jobtypes = 'all';}else{jobtypes=(index+1);}
+                        break;
+                    }
+                }
+                for(index=0; index<$scope.skill_types.length; index++){
+                    if($scope.skill_types[index].name===skill){
+                        if(index===11){skilltype = 'all';}else{skilltype=(index+1);}
+                        break;
+                    }
+                }
                 $location.path('/search').search({jobtype: jobtypes, skilltype: skilltype});
             };
 
@@ -45,7 +63,28 @@ app.controller('SearchController',
                 var params = $location.search();
                 var job_type = (params.jobtype);
                 var skill_type = (params.skilltype);
-
+                var index = 0;
+                for(index=0; index<$scope.job_types.length; index++){$scope.job_types[index].value=false;}
+                for(index=0; index<$scope.skill_types.length; index++){$scope.skill_types[index].value=false;}
+                if(job_type==='all'){$scope.job_types[3].value = true}
+                else{
+                    try {
+                        $scope.job_types[(job_type)].value = true
+                    }catch (err){
+                        console.log(err);
+                        console.log('Invalid job type:' + job_type);
+                    }
+                }
+                if(skill_type==='all'){$scope.skill_types[11].value = true}
+                else{
+                    try {
+                        $scope.skill_types[(job_type)].value = true
+                    }catch (err){
+                        console.log(err);
+                        console.log('Invalid skill type:' + skill_type);
+                    }
+                }
+                console.log('asdasmsojasoidfjkasfd: ' + $scope.skill_types[0].name);
                 var query1 = 'typeid=' + job_type;
                 var query2 = 'skillid=' + skill_type;
                 console.log("search url: http://localhost:3000/search/simplesearch?"+ query2 + "&" + query1);
@@ -64,12 +103,15 @@ app.controller('SearchController',
                         if(typeof stylists[i].description==="undefined"){description = stylists[i][0].description;}
                         else{ description = stylists[i].description;}
                         var types= '';
+                        console.log('---------------------');
+                        console.log(userTypes);
+                        console.log('---------------------');
                         for(var j=0; j<userTypes.length; j++){
                             if(userTypes[j].user_id===id){
                                 if(types===''){
-                                    types=$scope.job_types[(userTypes[j].job_id)-1];
+                                    types=$scope.job_types[(userTypes[j].job_id)-1].name;
                                 }else{
-                                    types=types + ', ' + $scope.job_types[(userTypes[j].job_id)-1];
+                                    types=types + ', ' + $scope.job_types[(userTypes[j].job_id)-1].name;
                                 }
                             }
                         }
