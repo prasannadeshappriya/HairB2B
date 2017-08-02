83663c23480a0deb665fb961e894ecb8412bcff7
Prasanna Deshappriya <prasanna.14@cse.mrt.ac.lk>
2017-08-02 16:49:05 +0530

    Implement casperjs test cases

---
+++ app/test/sample.test.js
@@ -0,0 +1,15 @@
+/**
+ * Created by prasanna_d on 8/2/2017.
+ */
+var casper = require('casper').create();
+casper.start('https://google.lk');
+
+casper.then(function() {
+    this.echo(this.getTitle());
+});
+
+// casper.thenOpen('http://phantomjs.org', function() {
+//     this.echo('Second Page: ' + this.getTitle());
+// });
+
+casper.run();
