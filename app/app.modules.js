c7ec7bd34a91c4a4845e3eb3ffbace1b623d7a70
Prasanna Deshappriya <prasanna.14@cse.mrt.ac.lk>
2017-08-08 16:32:35 +0530

    Heroku hosting

--- app/app.modules.js
+++ app/app.modules.js
@@ -9,6 +9,6 @@ var app = angular.module('app',[
     'signup',
     'ui.rCalendar'
 ])
-    .constant('host_url', 'http://localhost:3000/')
-    // .constant('host_url', 'https://hairbtob-server.herokuapp.com/')
+    // .constant('host_url', 'http://localhost:3000/')
+    .constant('host_url', 'https://hairbtob-server.herokuapp.com/')
 ;
